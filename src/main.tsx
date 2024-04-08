695a0dbfe8165085db1962354fda6f619382a044
Le Duc Thai <leducthai2001@gmail.com>
2024-04-08 22:04:16 +0700

    setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import { persistStore } from 'redux-persist'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 )
---
+++ src/store/store.ts
@@ -0,0 +1,41 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['clientApi', '_persist']
+}
+
+export const combineReducer = combineReducers({})
+
+export const rootReducers = (state: any, action: any) => {
+  // Reset the state to default when user logout
+  if (action.type === 'logout/logout') {
+    state = {} as RootState
+  }
+  return combineReducer(state, action as never)
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducers)
+
+export const store = configureStore({
+  devTools: true,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat()
+})
+setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
