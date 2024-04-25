8fb956e612a4415db2fed656e2c16ee177dd2900
Le Duc Thai <leducthai2001@gmail.com>
2024-04-25 10:55:19 +0700

    create Edit Description component

--- src/features/seller/components/profile/components/SellerOverview.tsx
+++ src/features/seller/components/profile/components/SellerOverview.tsx
@@ -4,6 +4,7 @@ import { ISellerDocument } from 'src/interfaces/seller.interface'
 
 import AboutMe from './overview/aboutme/AboutMe'
 import Certificates from './overview/certificates/Certificates'
+import Description from './overview/description/Description'
 import Language from './overview/language/Language'
 import SocialLinks from './overview/sociallinks/SocialLinks'
 
@@ -22,7 +23,9 @@ export default function SellerOverview({ sellerProfile, showEditIcons, setSeller
         <SocialLinks />
         <Certificates />
       </div>
-      <div className="w-full py-4 lg:w-2/3 lg:pl-4">Right Components</div>
+      <div className="w-full py-4 lg:w-2/3 lg:pl-4">
+        <Description />
+      </div>
     </SellerContext.Provider>
   )
 }
