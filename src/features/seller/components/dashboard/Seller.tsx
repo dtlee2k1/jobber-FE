9b7fe5a3328753acb6036910f8e008fae5447f13
Le Duc Thai <leducthai2001@gmail.com>
2024-04-28 17:50:21 +0700

    create SellerDashboard & DashboardMain components

--- src/features/seller/components/dashboard/Seller.tsx
+++ src/features/seller/components/dashboard/Seller.tsx
@@ -5,13 +5,14 @@ import { ISellerDocument } from 'src/interfaces/seller.interface'
 import { IReduxState } from 'src/interfaces/store.interface'
 import { useGetSellerByIdQuery } from 'src/services/seller.service'
 import DashBoardHeader from 'src/shared/header/components/DashBoardHeader'
+import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader'
 import { useAppSelector } from 'src/store/store'
 
 export default function Seller() {
   const header = useAppSelector((state: IReduxState) => state.header)
 
   const { sellerId } = useParams<string>()
-  const { data: sellerData } = useGetSellerByIdQuery(`${sellerId}`)
+  const { data: sellerData, isLoading } = useGetSellerByIdQuery(`${sellerId}`)
 
   const gigs: ISellerGig[] = []
   const pausedGigs: ISellerGig[] = []
@@ -20,9 +21,12 @@ export default function Seller() {
   return (
     <div className="w-screen">
       {header === 'sellerDashboard' && <DashBoardHeader />}
-      <div className="relative m-auto min-h-screen w-screen px-6 xl:container md:px-12 lg:px-6">
-        <Outlet context={{ seller: sellerData?.seller as ISellerDocument, gigs, pausedGigs, orders }} />
-      </div>
+      {isLoading && <CircularPageLoader />}
+      {!isLoading && (
+        <div className="relative m-auto min-h-screen w-screen px-6 xl:container md:px-12 lg:px-6">
+          <Outlet context={{ seller: sellerData?.seller as ISellerDocument, gigs, pausedGigs, orders }} />
+        </div>
+      )}
     </div>
   )
 }
